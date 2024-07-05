fcba548c02c368cb8c2168c6e0398b11266702bb
Reza Taghavi <r.taghavi1380@gmail.com>
2024-07-05 09:34:51 -0700

    deleted UserPromotions

--- portal/framework/src/main/java/com/taxi/framework/discount/repository/UserPromotionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.taxi.framework.discount.repository;
-
-import com.taxi.framework.discount.model.UserPromotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserPromotionRepository extends JpaRepository<UserPromotion, Long> { }
